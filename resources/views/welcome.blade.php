e954178230a20d0ef9803f08e1537ad37a72ce49
Mario <mario-tubay@hotmail.com>
2021-04-11 19:15:41 -0500

    Proyecto Terminado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,19 +3,23 @@
 @section('content')
 
 <div class="container">
-    @foreach ($datos as $value)
-    <div class="card" style="width: 18rem;">
-        <img src="{{$value->pfoto}} " class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">{{$value->pnombre}}</h5>
-          <p class="card-text">{{$value->cnombre}}</p>
-          <p class="card-text">{{$value->pdes}}</p>
-          <a href="{{route('update.producto',['id'=>$value->pid] )}}" class="btn btn-primary">Editar</a>
-          <a href="{{route('delete.producto',['id'=>$value->pid] )}} " class="btn btn-primary">Eliminar</a>
+<div class="row">
+      @foreach ($datos as $value)
+        <div class="col-sm">
+        <div class="card" style="width: 18rem;">
+            <img src="{{$value->pfoto}} " class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">{{$value->pnombre}}</h5>
+              <p class="card-text">{{$value->cnombre}}</p>
+              <p class="card-text">{{$value->pdes}}</p>
+              <a href="{{route('update.producto',['id'=>$value->pid] )}}" class="btn btn-primary">Editar</a>
+              <a href="{{route('delete.producto',['id'=>$value->pid] )}} " class="btn btn-primary">Eliminar</a>
+            </div>
+          </div>
         </div>
-      </div>
-    @endforeach
-    
+
+      @endforeach
+  </div>
 </div>
 
 
